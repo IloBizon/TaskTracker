ccbd99c075deabe437b647a87b659a1c24465d90
Гукасов Тимофей <ilobizonofficial@gmail.com>
2024-11-26 01:00:35 +0300

    Добавлен Dockerfile

--- TaskTracker/settings.py
+++ TaskTracker/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
-
+import os
+from dotenv import load_dotenv
 from django.conf.global_settings import AUTH_USER_MODEL
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^3&hy!k(n7*p8p=qn90zeb!)7b#&pazw8664rwk4n7s-dgk&v^'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
